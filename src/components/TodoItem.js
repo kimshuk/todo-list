cf2ff213a868688183c7f3897bbdc43c31ac2af0
developer <jeesoo2002@gmail.com>
2018-05-14 11:29:46 -0700

    revised todoitem comp

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,21 +3,26 @@ import "./TodoItem.css";
 
 class TodoItem extends Component {
   render() {
-    <div className="todo-item" onClick={() => onToggle(id)}>
-      <div
-        className="remove"
-        onClick={e => {
-          e.stopPropagation(); // prevent onToggle trigger
-          onRemove(id);
-        }}
-      >
-        &times;
+    const { text, checked, id, onToggle, onRemove } = this.props;
+
+    return (
+      <div className="todo-item" onClick={() => onToggle(id)}>
+        <div
+          className="remove"
+          onClick={e => {
+            console.log("remove");
+            e.stopPropagation(); // prevent onToggle trigger
+            onRemove(id);
+          }}
+        >
+          &times;
+        </div>
+        <div className={`todo-text ${checked ? "checked" : ""}`}>
+          <div>{text}</div>
+        </div>
+        {checked && <div className="check-mark" />}
       </div>
-      <div className={`todo-text ${checked && "checked"}`}>
-        <div>{text}</div>
-      </div>
-      {checked && <div className="check-mark" />}
-    </div>;
+    );
   }
 }
 
--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import TodoItem from "./TodoItem";
 
 class TodoItemList extends Component {
   render() {
     const { todos, onToggle, onRemove } = this.props;
 
-    return <div />;
+    return (
+      <div>
+        <TodoItem text="Hello" />
+        <TodoItem text="React" />
+        <TodoItem text="Nice to meet you" />
+      </div>
+    );
   }
 }
 
